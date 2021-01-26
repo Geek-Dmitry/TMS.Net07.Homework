5f89886d4372ae0abbbe02b482a2e6b15d0d4fb9
Dmitry Salata <salata.dzmitriy90@gmail.com>
2021-01-26 19:48:23 +0300

    Try.Parse method used

--- TMS.Net07.Homework.HelloWorld/TMS.Net07.Homework.HelloWorld/Hard.cs
+++ TMS.Net07.Homework.HelloWorld/TMS.Net07.Homework.HelloWorld/Hard.cs
@@ -7,32 +7,30 @@ class Hard
 
         int number;
         string enter;
+        bool inputIsNumber;
 
         Console.Title = "Dmitry Salata - TMS.Net07.Homework - Hard";
         Console.WriteLine("Please, enter any number!\n Enter number 0 for closing program");
         do
         {
-           enter = Console.ReadLine();
+            enter = Console.ReadLine();
+            inputIsNumber = Int32.TryParse(enter, out number);
 
-            try
+            if (inputIsNumber == true)
             {
-                number = Int32.Parse(enter);
-
                 if (number == 0)
-                {
-                    Console.WriteLine("You are enter number " + number + "!\n Entering number 0 is close this program.\n Good bye!");
-                }
+                    Console.WriteLine($"You are enter number {number}!\n Entering number 0 is close this program.\n Good bye!");
                 else
                 {
-                    Console.WriteLine("You are enter number " + number + "!\n Please, enter any other number.");
+                    Console.WriteLine($"You are enter number {number}!\n Please, enter any other number.");
                 }
             }
-            
-            catch
+            else 
             {
-                Console.WriteLine("You are enter " + enter + " and it is not a number!\n Please, enter only a numbers.");
+                Console.WriteLine($"You are enter {enter} and it is not a number!\n Please, enter only a numbers.");
+                number = 1;
             }
         }
-        while (enter != "0");
+        while (number != 0);
     }
 }
