3d5104c3b2e6c614cff35e9701f0e90f07c75078
Dmitry Salata <salata.dzmitriy90@gmail.com>
2021-01-30 18:12:48 +0300

    middle level done

--- TMS.Net07.HomeworkDateToDayOfWeeks/TMS.Net07.Homework2 - middle/Program.cs
+++ TMS.Net07.HomeworkDateToDayOfWeeks/TMS.Net07.Homework2 - middle/Program.cs
@@ -10,7 +10,27 @@ namespace TMS.Net07.Homework2___middle
     {
         static void Main(string[] args)
         {
+            string input;
+            DateTime inputDate;
 
+            Console.Title = "Dmitry Salata - TMS.Net07.Homework2 - level not slave, but not master (middle)";
+            Console.WriteLine("Please, enter the date in format DD.MM.yyyy or DD/MM/yyyy.");
+
+            input = Console.ReadLine();
+
+            for (;String.Equals(input.ToLower(), "exit") == false; input = Console.ReadLine()) 
+            {
+               
+                if (DateTime.TryParse(input, out inputDate) == true)
+                {
+                    Console.WriteLine(inputDate.ToString($"\n{input} - dddd", new System.Globalization.CultureInfo("en-EN")));
+                }
+                else
+                {
+                    Console.WriteLine($"\nYou are enter a {input} and it is not a date or format of date are not acceptable. " +
+                        $"\nPlease enter the date in format DD.MM.yyyy or DD/MM/yyyy");
+                }
+            }
         }
     }
 }
