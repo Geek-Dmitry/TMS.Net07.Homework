a915b79c205d749d879d322a9cf85b24dd82c271
Dmitry Salata <salata.dzmitriy90@gmail.com>
2021-02-08 17:21:18 +0300

    second and third task for factorial and fibonachi done

---
+++ TMS.Net07.Homework.Algoritms/TMS.Net07.Homework.Algoritms - factorial recursion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.Algoritms___factorial_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string errorMessage = "Please, check your input";
+            while (true)
+            {
+                int index;
+                string input = Console.ReadLine();
+                //check input value
+                if (input == "exit")
+                {
+                    return;
+                }
+                bool isOk = Int32.TryParse(input, out index);
+                if (!isOk)
+                {
+                    Console.WriteLine($"{errorMessage}");
+                }
+                int result = resultValue(index);
+                Console.WriteLine($"{Environment.NewLine}{result}");
+            }
+        }
+        //method for factorial
+        static int resultValue (int index)
+        {
+            if (index == 0)
+            {
+                return 1;
+            }
+            else return index * resultValue(index - 1);
+        }
+    }
+}
---
+++ TMS.Net07.Homework.Algoritms/TMS.Net07.Homework.Algoritms - fibs number recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TMS.Net07.Homework.Algoritms___fibs_number_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string errorMessage = "Please, check your input";
+            while (true)
+            {
+                int index;
+                string input = Console.ReadLine();
+                //check input value
+                if (input == "exit")
+                {
+                    return;
+                }
+                bool isOk = Int32.TryParse(input, out index);
+                if (!isOk)
+                {
+                    Console.WriteLine($"{errorMessage}");
+                }
+                int result = resultValue(index);
+                Console.WriteLine($"{Environment.NewLine}{result}");
+            }
+        }
+        //method for factorial
+        static int resultValue(int index)
+        {
+            if (index == 1 || index == 2)
+            {
+                return 1;
+            }
+            else return resultValue(index - 1) + resultValue(index - 2);
+        }
+    }
+}
